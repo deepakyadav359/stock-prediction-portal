8a04928967e8d8fdc916d0f2fad972efe3b7db99
Deepak Yadav <deepakradheshyamyadav@gmail.com>
2026-06-25 11:58:10 +0530

    registration page

--- frontend-react/src/components/Main.jsx
+++ frontend-react/src/components/Main.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
+import Header from './Header'
 import Button from './Button'
+import Footer from './Footer'
 
 const Main = () => {
   return (
@@ -9,6 +11,7 @@ const Main = () => {
             <h1 className='text-light'>Stock Prediction Portal</h1>
             <p className='text-light lead'>This stock prediction application utilizees machine learning techniques, specifically employing Keras, and LSTM model, integrated within the django framework.It forecasts future stock prices by analyzing 100-days and 200- days moving averages, essential indicators widely used by stock analysts to inform trading and investment decisions.</p>
             <Button text="Login" class="btn-outline-info"  />
+            
         </div>
       </div>
     </>
